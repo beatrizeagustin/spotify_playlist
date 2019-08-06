d70377694343f41af45b5b79e0c21fc36396cd7f
Beatrize <heybeatrize@gmail.com>
2019-08-06 11:11:38 -0700

    added data

--- spotify_playlist.py
+++ spotify_playlist.py
@@ -1,5 +1,32 @@
 playlist = {
 	'title': 'pop songs',
 	'author': 'beatrize',
-	'songs': [] 
+	'songs': [
+		{
+		'title': 'song',
+		'artist': [
+			'artist_name',
+			 ],
+		'duration': 2.5
+		},
+		{
+		'title': 'song2',
+		'artist': [
+			'artist_name',
+			 ],
+		'duration': 2.5
+		},
+		{
+		'title': 'song3',
+		'artist': [
+			'artist_name',
+			 ],
+		'duration': 2.5
+		}
+	] 
 }
+
+total_legnth = 0
+for song in playlist['songs']:
+	total_legnth += song['duration']
+print(total_legnth)
